6bbda297b0ea60aafd106323ecb17572ed99d35c
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-04-08 08:28:25 +0430

    feat(frontend): initialize frontend with nextjs

---
+++ frontend/pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  align-items: center;
+  background-color: #31394C;
+  color: #FDFDFD;
+  display: flex;
+  font-family: sans-serif;
+  font-size: 28px;
+  font-weight: 700;
+  height: 100vh;
+  justify-content: center;
+  width: 100vw;
+`
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Demo</title>
+        <meta name="description" content="CI/CD with GitHub Actions" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container>
+        <p>CI/CD with GitHub Actions Demo! Pending</p>
+      </Container>
+    </>
+  )
+}
+
+export default Home
